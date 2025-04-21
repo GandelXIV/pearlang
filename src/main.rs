c9b54deae398bdd8d57bf84870fc50cac1590455
Filip Chovanec <chovafil@fit.cvut.cz>
2025-04-21 18:43:22 +0200

    string parsing

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ struct Token {
 }
 
 impl From<String> for Token {
+    // only handles Number and Keyword, not Text
     fn from(s: String) -> Self {
         let cat = match s.chars().nth(0).expect("token should not be empty") {
-            '\"' => TokenCategory::Text,
             '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => TokenCategory::Number,
             _ => TokenCategory::Keyword,
         };
@@ -54,31 +54,47 @@ type Block = Vec<Statement>;
 
 fn parse(raw: String) -> Block {
     let mut raw: String = raw.chars().rev().collect();
-    raw.push('\n');
+    raw.insert(0, '\n');
 
     let mut block = Block::default();
     let mut statement: Option<Statement> = None;
     let mut word: Option<String> = None;
+
+    let mut isstring = false;
+    let mut islaststring = false;
     while let Some(chr) = raw.pop() {
         match chr {
+            '"' => {
+                isstring = !isstring;
+                if !isstring {
+                    islaststring = true;
+                }
+            }
+            x if isstring => match &mut word {
+                Some(word) => word.push(x),
+                None => word = Some(x.to_string()),
+            },
             ' ' => {
-                println!("{:?} {:?}", statement, word);
                 if let Some(word) = word.take() {
                     match &mut statement {
-                        Some(statement) => {
-                            statement.arg.push(Token::from(word));
-                        }
+                        Some(statement) => statement.arg.push(if islaststring {
+                            Token {
+                                name: word,
+                                category: TokenCategory::Text,
+                            }
+                        } else {
+                            Token::from(word)
+                        }),
                         None => statement = Some(Statement::new(word)),
                     }
                 }
+                islaststring = false;
             }
             '\n' => {
                 if let Some(mut statement) = statement.take() {
                     if let Some(word) = word.take() {
                         statement.arg.push(Token::from(word));
                     }
-                    println!("{:?} {:?}", statement, word);
-
                     block.push(statement);
                 }
             }
@@ -96,9 +112,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_base() {
+    fn test_line_and_spaces_stmnt() {
         assert_eq!(
-            parse("echo abc def\n".to_string()),
+            parse("echo abc    def\n".to_string()),
             vec![Statement {
                 cmd: Token {
                     category: TokenCategory::Keyword,
@@ -118,9 +134,36 @@ mod tests {
             }]
         );
     }
+
+    #[test]
+    fn test_strings_and_tokencats() {
+        assert_eq!(
+            parse("echo \"Hello World!\" \"123\" 456".into()),
+            vec![Statement {
+                cmd: Token {
+                    category: TokenCategory::Keyword,
+                    name: "echo".to_string()
+                },
+                arg: vec![
+                    Token {
+                        category: TokenCategory::Text,
+                        name: "Hello World!".to_string()
+                    },
+                    Token {
+                        category: TokenCategory::Text,
+                        name: "123".to_string()
+                    },
+                    Token {
+                        category: TokenCategory::Number,
+                        name: "456".to_string()
+                    }
+                ],
+                block: vec![]
+            }]
+        )
+    }
 }
 
 fn main() {
-    parse("echo abc def\n".to_string());
     println!("Hello, world!");
 }
